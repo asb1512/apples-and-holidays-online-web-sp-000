f3ba6a44da3af9a5457a52a96899bd20368f503e
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-24 01:09:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -109,6 +109,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday_key, supply|
       if supply.include?("BBQ") == true
+        holiday_key
       end
     end
   end
